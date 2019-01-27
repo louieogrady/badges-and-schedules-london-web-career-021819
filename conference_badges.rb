4f4e8d495c9603df5547cd77452962a61ddf5a18
Louie O'Grady <l.ogrady@gmail.com>
2019-01-27 19:33:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.map {|x| badge_maker(x)} # using map or collect
+end
+
+def assign_rooms(name)
+  name.each_with_index.map { |name, room| "Hello, #{name}! You'll be assigned to room #{room+1}!"}
+end
+
+def printer(name)
+  batch_badge_creator
+  assign_rooms
+end
