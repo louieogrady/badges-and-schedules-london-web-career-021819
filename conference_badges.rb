be3549930c69fc9f20820c829399afe8798896ad
Louie O'Grady <l.ogrady@gmail.com>
2019-01-27 19:38:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def printer(name)
   batch_badge_creator(name).each do |badge|
     puts badge
   end
-  assign_rooms.each do |rooms|
+  assign_rooms(name).each do |rooms|
     puts rooms
 end
 end
